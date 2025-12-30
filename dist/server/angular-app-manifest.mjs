43bc66ecb1843e809940eed024e24f3511ab5eb7
Karri Narendra Reddy <karrinarendra.reddy@nielseniq.com>
2025-12-31 02:12:43 +0530

    Hotfix 2.0 Release 2.0

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 22773, hash: '7a816427989510d73757c22a884f8434f3bdb1cc63789bc317c39766fb05c1dc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14495, hash: 'a3bd892851081799d5029e5da365dd52dd1b72c96f090139b1380b1de94f6edc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 112131, hash: '590ae76c7ae3eb21abd91c7abb31b4a1452ca66e98f172bf4297629365ed8874', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 182785, hash: '4c02ad4f4eac88a4c64f64b33deebf6dbf95f8a2bfd49f505966cfdd09440f32', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 22773, hash: 'd10c4b3edb8cdd7fd28c8858a03a33d709c8a4077c00f359919cee3fe6b30eb5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14495, hash: '8941539dca98c30ea7e0dc955b764c9d89e277c67637bd8c032294a91bae97ed', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 112131, hash: '9ae079313f9b306a80e3ee7a88eeb9ff8ca780f27e3ea1860f920da73f5302d0', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 182785, hash: 'd3f98337ea1169d36f7c32b0b74e6ab59b05fba22d7e0fa893a1670b19e2e970', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-6NAANYBT.css': {size: 11680, hash: '3pZljaKCuWo', text: () => import('./assets-chunks/styles-6NAANYBT_css.mjs').then(m => m.default)}
   },
 };
