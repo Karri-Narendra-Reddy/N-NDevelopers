8029b1b23b0825a0cd3c46d1d4ced5662784d3e1
Karri Narendra Reddy <karrinarendra.reddy@nielseniq.com>
2026-01-02 12:00:05 +0530

    Updated the contact details

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 22773, hash: 'acdf7c9a59fa90a72bce5153fd819eb796f9623f4b4e7955c93d0d0a86f213dd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14495, hash: 'e4662034185e477e4187c62eefcbb4d66b80d94264261931822246ec116a02a3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 112131, hash: '4082fc28298de2ad33dff779524197aeea43d416f7ca78b9b250859fcfc273e6', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 187108, hash: '7043ad337146ce082f69574f6b57687cb41ecc431b796eaf4730131b91bc71a6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 22773, hash: '6c9ca16f5841f134378b2c303230de44e203e28ae93c8986c08cff5c5ec7b922', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14495, hash: '70f18c99e7cf7deeb208e061b3cff818041750010a7204a2a6ef51d14c647fc2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 112197, hash: '2bd3ba9ca876e0fb217f3717e271be0935857ed464d8524f37e7f9363d88d918', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 187231, hash: '38fd9812ac06ba8353b23a8c4a912ed23544052cbddad39312a21560b378447a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-6NAANYBT.css': {size: 11680, hash: '3pZljaKCuWo', text: () => import('./assets-chunks/styles-6NAANYBT_css.mjs').then(m => m.default)}
   },
 };
