1ca010156da9771891d90a5398297ba95cd10933
Karri Narendra Reddy <karrinarendra.reddy@nielseniq.com>
2026-01-02 12:18:15 +0530

    Fixed the insta navigation bug

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 22773, hash: '6c9ca16f5841f134378b2c303230de44e203e28ae93c8986c08cff5c5ec7b922', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14495, hash: '70f18c99e7cf7deeb208e061b3cff818041750010a7204a2a6ef51d14c647fc2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 112197, hash: '2bd3ba9ca876e0fb217f3717e271be0935857ed464d8524f37e7f9363d88d918', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 187231, hash: '38fd9812ac06ba8353b23a8c4a912ed23544052cbddad39312a21560b378447a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 22773, hash: '071b0b8216503b2aa5279d6b9df8749a5697a26503cb8e379c4666794b96f9a1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14495, hash: 'a18ffbb1ec7d0d4b354929b6a54c6c31cc8025245bd5aeca412843658029afbb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 187226, hash: '89ee454f8d32a875c33f233150f377a4f98ca47a028aee63f1543912cedbcef9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 112197, hash: '3892aa3f8c91bd732c2e3aee9bf4dcf2e7a616ff03c614ab162e4e3a5209e90f', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
     'styles-6NAANYBT.css': {size: 11680, hash: '3pZljaKCuWo', text: () => import('./assets-chunks/styles-6NAANYBT_css.mjs').then(m => m.default)}
   },
 };
